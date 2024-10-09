21e8c3f9d52bc655014c48501806686f345be7d7
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-09 15:29:11 -0400

    completed pay rules exercise

---
+++ IfScripts/pay-rules.js
@@ -0,0 +1,30 @@
+// var payRate = 12.50;
+// var hoursWorked = 20;
+// let grossPay = 0
+// if (hoursWorked < 40)
+// {grossPay = payRate*hoursWorked}
+// else {grossPay = payRate * 40 + (hoursWorked - 40) * payRate * 1.5}
+// console.log("$" + grossPay);
+
+
+
+
+// var payRate = 25;
+// var hoursWorked = 40
+// let grossPay = 0
+// if (hoursWorked < 40)
+// {grossPay = payRate * hoursWorked}
+// else {grossPay = payRate * 40 + (hoursWorked - 40) * payRate * 1.5}
+// console.log("$" + grossPay);
+
+
+
+
+// var payRate = 17.30;
+// var hoursWorked = 45;
+// var regHours = 40;
+// var overtimeHours = 5;
+// let grossPay = 0; 
+// if (hoursWorked > 40);
+// {grossPay = (payRate * 1.5) * overtimeHours + (regHours * payRate)}
+// console.log("$" + grossPay);
---
+++ numeric-conversions/conversion-tests.js
@@ -0,0 +1,50 @@
+//Description: This script tests various numeric conversion techniques
+// Nicholas Lynch 
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5"
+
+//a = parseInt(a);
+//console.log(parseInt(a));
+//b = parseInt(b)
+//console.log(parseInt(b));
+//c = parseInt(c)
+//console.log(parseInt(c));
+//d = parseInt(d)
+//console.log(parseInt(d));
+
+
+//a = parseFloat(a)
+//console.log(parseFloat(a));
+//b = parseFloat(b)
+//console.log(parseFloat(b));
+//c = parseFloat(c)
+//console.log(parseFloat(c));
+//d = parseFloat(d)
+//console.log(parseFloat(d));
+
+
+//a = Number(a)
+//console.log(Number(a));
+//b = Number(b)
+//console.log(Number(b));
+//c = Number(c)
+//console.log(Number(c));
+//d = Number(d)
+//console.log(Number(d));
+
+
+//a = +a;
+//console.log(+a);
+//b = +b;
+//console.log(+b);
+//c = +c;
+//console.log(+c);
+//d = +d;
+//console.log(+d);
+
+var numberAsString =  "12"
+var number = Number(numberAsString);
+console.log(number);
