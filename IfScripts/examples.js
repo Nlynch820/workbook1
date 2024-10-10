7ad65601f62c1d6267c3f4ff52c9b931333f0cd3
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-10 10:25:41 -0400

    if else statement

---
+++ IfScripts/examples.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var number = 11308
+
+if (number%2==0)
+    console.log("The number is even.");
+    else {
+        console.log("The number is odd.");
+        
+    }
+    console.log("This is how we do it.");
+    
