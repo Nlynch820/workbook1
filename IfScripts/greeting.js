b34f00fe394a5e62e1ce33a01f29924d2e55430c
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-10 10:41:04 -0400

    greetings exercise

---
+++ IfScripts/greeting.js
@@ -0,0 +1,16 @@
+"use strict"
+
+var hours = 9;
+ 
+
+if (hours < 10) {
+    console.log("Good Morning");
+}
+else if (hours < 17) {
+    console.log("Good Day");
+    
+}
+else if (hours > 17) {
+    console.log("Good Evening");
+     
+}
